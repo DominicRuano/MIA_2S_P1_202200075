a48d8a751c05d8e076203f108707241a620e99a3
DominicRuano <dojuparupe1975@gmail.com>
2024-09-11 16:58:58 -0600

    Refactor disk creation commands in script.bash and mkDisk.go

--- Pruebas/script.bash
+++ Pruebas/script.bash
@@ -1,13 +1,15 @@
 #Crea disco mia1
 mkdisk    -size=1000   -unit=K -path=/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/Disco1.mia
 
-# Crea disco mia2
+#Crea disco mia2
 mkdisk   -Size=2000   -unit=K -path="/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/Disco2.mia"
 
 #Crea disco mia3
 mkdisk -Size=3000 -unit=K -path="/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/cosas mias/Disco3.mia"
 
 #Elimina los 3 discos
+#rmdisk -path=/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/Disco1.mia -path="/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/Disco2.mia" -path="/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/cosas mias/Disco3.mia"
+
 rmdisk -path=/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/Disco1.mia
 rmdisk -path="/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/Disco2.mia"
 rmdisk -path="/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/cosas mias/Disco3.mia"
--- backend/Commands/rmDisk.go
+++ backend/Commands/rmDisk.go
@@ -9,14 +9,22 @@ import (
 )
 
 func RMDisk(tokens []string) string {
+	var response string
 	// Expresión regular para encontrar los parámetros del comando mkdisk
 	Regex := `(?i)-path="[^"]+"|-path=[^\s]+`
 
+	// Verificar si el comando tiene al menos un parametro
 	if len(tokens) < 1 {
 		return "Error: Comando rmdisk requiere al menos un parametro (path).\n"
 	}
 
+	// Obtener los parametros del comando
 	tokens = utils.ParseParametros(tokens, Regex)
+	if len(tokens) < 1 {
+		return "Error: No se detecto el path del archivo, por favor verifica la entrada.\n"
+	}
+
+	// Iterar sobre los tokens para ejecutar el comando
 	for _, token := range tokens {
 		partes := strings.SplitN(token, "=", 2) // Separa el token en partes
 
@@ -26,11 +34,11 @@ func RMDisk(tokens []string) string {
 		// Intentar eliminar el archivo
 		err := os.Remove(strings.ReplaceAll(partes[1], "\"", ""))
 		if err != nil {
-			return fmt.Sprintln("Error al eliminar el archivo:", err)
+			response += fmt.Sprintln("Error al eliminar el archivo:", err)
 		} else {
-			return fmt.Sprintf("Archivo %s eliminado exitosamente. \n", fileName)
+			response += fmt.Sprintf("Archivo %s eliminado exitosamente. \n", fileName)
 		}
 	}
 
-	return "Ocurrio un Problema al detectar rmdisk.\n"
+	return response
 }

41012d339e2de6f69a4ba782441394643c056164
DominicRuano <dojuparupe1975@gmail.com>
2024-09-11 15:20:35 -0600

    Add disk creation commands to script.bash

---
+++ Pruebas/script.bash
@@ -0,0 +1,3 @@
+mkdisk -Size=3000 -unit=K -path=/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/Disco1.mia
+
+mkdisk -Size=5000 -unit=K -path="/home/drop/Documentos/U/Lab_MIA/MIA_2S_P1_202200075/Pruebas/Disco2.mia"

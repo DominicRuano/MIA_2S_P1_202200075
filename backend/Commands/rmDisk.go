56fc83d6958dc8bc436a7cb919906e6db6a03f13
DominicRuano <dojuparupe1975@gmail.com>
2024-09-11 16:46:19 -0600

    Refactor rmDisk command to handle multiple parameters and improve error handling

--- backend/Commands/rmDisk.go
+++ backend/Commands/rmDisk.go
@@ -1,5 +1,36 @@
 package commands
 
+import (
+	utils "Backend/Utils"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 func RMDisk(tokens []string) string {
-	return "Se detecto RMDISK\n"
+	// Expresión regular para encontrar los parámetros del comando mkdisk
+	Regex := `(?i)-path="[^"]+"|-path=[^\s]+`
+
+	if len(tokens) < 1 {
+		return "Error: Comando rmdisk requiere al menos un parametro (path).\n"
+	}
+
+	tokens = utils.ParseParametros(tokens, Regex)
+	for _, token := range tokens {
+		partes := strings.SplitN(token, "=", 2) // Separa el token en partes
+
+		// Obtener solo el nombre del archivo
+		fileName := filepath.Base(strings.ReplaceAll(partes[1], "\"", ""))
+
+		// Intentar eliminar el archivo
+		err := os.Remove(strings.ReplaceAll(partes[1], "\"", ""))
+		if err != nil {
+			return fmt.Sprintln("Error al eliminar el archivo:", err)
+		} else {
+			return fmt.Sprintf("Archivo %s eliminado exitosamente. \n", fileName)
+		}
+	}
+
+	return "Ocurrio un Problema al detectar rmdisk.\n"
 }

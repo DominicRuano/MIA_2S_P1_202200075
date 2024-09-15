06af97c62ded78ac461b1a767e7a5249a4de39e3
DominicRuano <dojuparupe1975@gmail.com>
2024-09-14 18:08:29 -0600

    Refactor MBR.go to handle partition fitting and index retrieval

--- backend/Structs/MBR.go
+++ backend/Structs/MBR.go
@@ -1,9 +1,11 @@
 package structs
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -81,4 +83,148 @@ func (mbr *MBR) PrintPartitions() {
 		fmt.Printf("  Correlative: %d\n", partition.Part_correlative)
 		fmt.Printf("  ID: %d\n", partition.Part_id)
 	}
+}
+
+/*
+GetPartitionIndex devuelve el índice de la partición que cumple con el tipo de ajuste especificado
+
+BF: Best Fit
+FF: First Fit
+WF: Worst Fit
+
+Si no se encuentra una partición disponible, devuelve -1 y un error
+*/
+func (mbr *MBR) GetPartitionIndex(Type string) (int, error) {
+	if mbr.PartitionAvailable() == -1 {
+		return -1, fmt.Errorf("no hay particiones disponibles en el disco")
+	}
+
+	if strings.ToUpper(Type) == "BF" {
+		return mbr.getBestFit()
+	} else if strings.ToUpper(Type) == "FF" {
+		return mbr.getFirstFit()
+	} else if strings.ToUpper(Type) == "WF" {
+		return mbr.getWorstFit()
+	}
+	return -1, fmt.Errorf("getPartitionIndex, no detecto un tipo de particion correcto")
+}
+
+/*
+PartitionAvailable devuelve 1 si hay una partición disponible, -1 en caso contrario
+*/
+func (mbr *MBR) PartitionAvailable() int {
+	for _, partition := range mbr.Mbr_partitions {
+		if partition.Part_start == -1 && partition.Part_size == -1 {
+			return 1
+		}
+	}
+	return -1
+}
+
+/*
+getBestFit devuelve el índice de la partición con el mejor ajuste
+
+# El mejor ajuste es la partición con el tamaño más pequeño que sea mayor o igual al tamaño de la partición a crear
+
+Si no se encuentra una partición disponible, devuelve -1 y un error
+*/
+func (mbr *MBR) getBestFit() (int, error) {
+
+	for i, partition := range mbr.Mbr_partitions {
+		if partition.Part_start == -1 {
+			return i, nil
+		}
+	}
+
+	return -1, fmt.Errorf("no se pudo encontrar una particion BF ")
+}
+
+/*
+getFirstFit devuelve el índice de la primera partición disponible
+
+Si no se encuentra una partición disponible, devuelve -1 y un error
+*/
+func (mbr *MBR) getFirstFit() (int, error) {
+	for i, partition := range mbr.Mbr_partitions {
+		if partition.Part_start == -1 {
+			return i, nil
+		}
+	}
+
+	return -1, fmt.Errorf("error: no se pudo encontrar una particion FF ")
+}
+
+/*
+getWorstFit devuelve el índice de la partición con el peor ajuste
+
+# El peor ajuste es la partición con el tamaño más grande que sea mayor o igual al tamaño de la partición a crear
+
+Si no se encuentra una partición disponible, devuelve -1 y un error
+*/
+func (mbr *MBR) getWorstFit() (int, error) {
+	for i, partition := range mbr.Mbr_partitions {
+		if partition.Part_start == -1 {
+			return i, nil
+		}
+	}
+
+	return -1, fmt.Errorf("error: no se pudo encontrar una particion WF ")
+}
+
+/*
+Funcion imcompleta, solo suma los valores de las particiones consecutivas.
+*/
+func (mbr *MBR) CalcularStart(index int) int {
+	var start int = int(binary.Size(mbr))
+
+	for i := 0; i < index; i++ {
+		if mbr.Mbr_partitions[i].Part_start != -1 {
+			start += int(mbr.Mbr_partitions[i].Part_size)
+		}
+	}
+	return start
+}
+
+func (mbr *MBR) Verifyname(name string) int {
+	for _, partition := range mbr.Mbr_partitions {
+		// Convertimos el nombre de la partición quitando cualquier carácter nulo
+		partitionName := string(bytes.Trim(partition.Part_name[:], "\x00"))
+
+		if partitionName == name {
+			return -1 // Existe
+		}
+	}
+	return 1 // No existe
+}
+
+/*
+Retorna true si existe una partición extendida en el disco
+*/
+func (mbr *MBR) ExistExtended() bool {
+	for _, partition := range mbr.Mbr_partitions {
+		if partition.Part_type[0] == 'E' {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+CabeParticion verifica si una partición cabe en el disco, solo implementada para FF.
+*/
+func (mbr *MBR) CabeParticion(Size int, Start int) bool {
+	// Calcular el final de la partición
+	end := Start + Size
+
+	// Verificar si la partición cabe en el disco
+	return end <= int(mbr.Mbr_size)
+}
+
+func (mbr *MBR) GetextendedPartitionIndex() int {
+	for i, partition := range mbr.Mbr_partitions {
+		if partition.Part_type[0] == 'E' {
+			return i
+		}
+	}
+	return -1
 }

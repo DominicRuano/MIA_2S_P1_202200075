af489a40cfdc40a684087429e9e76d60dd33267b
DominicRuano <dojuparupe1975@gmail.com>
2024-09-15 23:36:03 -0600

    Refactor Rep function to handle disk reports

--- backend/Commands/rep.go
+++ backend/Commands/rep.go
@@ -57,17 +57,7 @@ func Rep(tokens []string) string {
 
 		switch strings.ToLower(partes[0]) { // Switch para manejar los parametros
 		case "-name":
-			if strings.ToUpper(partes[1]) == "MBR" ||
-				strings.ToUpper(partes[1]) == "DISK" ||
-				strings.ToUpper(partes[1]) == "INODE" ||
-				strings.ToUpper(partes[1]) == "BLOCK" ||
-				strings.ToUpper(partes[1]) == "BM_INODE" ||
-				strings.ToUpper(partes[1]) == "BM_BLOCK" ||
-				strings.ToUpper(partes[1]) == "SB" ||
-				strings.ToUpper(partes[1]) == "FILE" ||
-				strings.ToUpper(partes[1]) == "LS" {
-				Cmd.Rep_name = partes[1]
-			}
+			Cmd.Rep_name = partes[1]
 		case "-path":
 			Cmd.Rep_pathDestino = strings.ReplaceAll(partes[1], "\"", "")
 		case "-id":
@@ -99,10 +89,53 @@ func Rep(tokens []string) string {
 	switch strings.ToLower(Cmd.Rep_name) {
 	case "mbr":
 		return MBRReporte(Cmd)
+	case "disk":
+		return DISKReporte(Cmd)
 	default:
-		return "Error: Nombre de reporte no reconocido.\n"
+		return fmt.Sprintf("Error: Nombre de reporte %s no reconocido.\n", Cmd.Rep_name)
 	}
 
+}
+
+func DISKReporte(Cmd *Rep_st) string {
+	// Crea una variable para almacenar el disco.
+	var mbr structs.MBR
+
+	// Obtiene el mbr del archivo
+	mbr.DeserializeMBR(Cmd.Rep_pathDisk)
+
+	// Obtener la extensión del archivo
+	ext := filepath.Ext(Cmd.Rep_pathDestino)
+
+	// Path final del archivo sin la extensión
+	finalPath := strings.TrimSuffix(Cmd.Rep_pathDestino, ext)
+
+	// Obtener el directorio del path (sin el archivo)
+	dir := filepath.Dir(Cmd.Rep_pathDestino)
+
+	// Crear todas las carpetas necesarias si no existen
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return fmt.Sprintf("Error al crear directorios: %v\n", err)
+	}
+
+	// Generar el archivo DOT
+	err = GenerateDotFileDISK(mbr, finalPath+".dot", Cmd.Rep_pathDisk)
+	if err != nil {
+		return fmt.Sprintln("Error:", err)
+	}
+
+	// Ejecutar el comando DOT
+	err = ExecuteDot(finalPath+".dot", Cmd.Rep_pathDestino)
+	if err != nil {
+		return fmt.Sprintln("Error:", err)
+	}
+
+	// Intentar eliminar el archivo
+	//os.Remove(finalPath + ".dot")
+
+	// Mensaje de éxito
+	return fmt.Sprintf("REP: Reporte Disk Para el Id %s generado con éxito.\n", Cmd.Rep_id)
 }
 
 func MBRReporte(Cmd *Rep_st) string {
@@ -128,7 +161,7 @@ func MBRReporte(Cmd *Rep_st) string {
 	}
 
 	// Generar el archivo DOT
-	err = GenerateDotFile(mbr, finalPath+".dot", Cmd.Rep_pathDisk)
+	err = GenerateDotFileMBR(mbr, finalPath+".dot", Cmd.Rep_pathDisk)
 	if err != nil {
 		return fmt.Sprintln("Error:", err)
 	}
@@ -147,7 +180,171 @@ func MBRReporte(Cmd *Rep_st) string {
 }
 
 // Función para generar el archivo DOT con la estructura del MBR
-func GenerateDotFile(mbr structs.MBR, filePath string, discPath string) error {
+func GenerateDotFileDISK(mbr structs.MBR, filePath string, discPath string) error {
+	// Crear el archivo .dot
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("error al crear el archivo DOT: %v", err)
+	}
+	defer file.Close()
+
+	disconame := filepath.Base(discPath)
+
+	// Escribir el contenido del archivo DOT
+	_, err = file.WriteString(`
+digraph G {
+    node [shape=plaintext];
+    A [label=<
+        <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
+            <TR>
+                <TD COLSPAN="999" BORDER="0"><B>` + disconame + `</B></TD>
+            </TR>
+            <TR>
+				<TD ROWSPAN="2">MBR</TD>`)
+
+	if err != nil {
+		return fmt.Errorf("error el escribir el tamaño del MBR en el archivo DOT: %v", err)
+	}
+
+	// Indice de la partición extendida
+	ExtendidaIndex := -1
+
+	// Indice de la ultima partición
+	LastIndex := -1
+
+	// Escribir las particiones en el archivo DOT
+	for i, partition := range mbr.Mbr_partitions {
+		if partition.Part_type[0] == 'P' {
+
+			// Actualiza el indice de la ultima partición
+			LastIndex = i
+
+			// Calcular el Porcentaje en BYTES que ocupa la partición en el disco
+			partWeight := float64(partition.Part_size) * 100 / float64(mbr.Mbr_size)
+
+			// Escribir la partición en el archivo DOT
+			_, err := file.WriteString(`
+				<TD ROWSPAN="2">Primaria<BR/><FONT POINT-SIZE="10" COLOR="red">` + fmt.Sprintf("%.2f", partWeight) + `%` + ` del disco</FONT></TD>`)
+			if err != nil {
+				return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+			}
+		} else if partition.Part_type[0] == 'E' {
+
+			// Actualiza el indice de la ultima partición
+			LastIndex = i
+
+			// Guarda el indice de la partición extendida
+			ExtendidaIndex = i
+			println("ExtendidaIndex: ", ExtendidaIndex)
+
+			// Calcula el ROWSPAN de la particion extendida
+			ROWSPAN := 2
+
+			// Crear una variable para almacenar los datos leídos
+			var ebr structs.EBR
+
+			// Obtiene el ebr del archivo
+			ebr.DeserializeEBR(discPath, int64(partition.Part_start))
+
+			for ebr.Ebr_next != -1 {
+				ROWSPAN += 2
+				// Obtiene el proximo ebr del archivo
+				ebr.DeserializeEBR(discPath, int64(ebr.Ebr_next))
+			}
+
+			// Calcular el Porcentaje en BYTES que ocupa la partición en el disco
+			partWeight := float64(partition.Part_size) * 100 / float64(mbr.Mbr_size)
+
+			// Escribir la partición en el archivo DOT
+			_, err := file.WriteString(`
+					<TD COLSPAN="` + fmt.Sprint(ROWSPAN) + `">Extendida<BR/><FONT POINT-SIZE="10" COLOR="red">` + fmt.Sprintf("%.2f", partWeight) + `%` + ` del disco</FONT></TD>`)
+			if err != nil {
+				return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+			}
+		}
+	}
+
+	// Calcular el Porcentaje en BYTES que ocupa la ultima partición en el disco
+	ultimaParticion := mbr.Mbr_size - (mbr.Mbr_partitions[LastIndex].Part_start + mbr.Mbr_partitions[LastIndex].Part_size)
+	partWeight := float64(ultimaParticion) * 100 / float64(mbr.Mbr_size)
+
+	// Escribir la partición en el archivo DOT
+	_, err = file.WriteString(`
+				<TD ROWSPAN="2">Libre<BR/><FONT POINT-SIZE="10" COLOR="red">` + fmt.Sprintf("%.2f", partWeight) + `%` + ` del disco</FONT></TD>`)
+	if err != nil {
+		return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+	}
+
+	// Escribir en el archivo DOT
+	_, err = file.WriteString(`
+		    </TR>
+            <TR>`)
+	if err != nil {
+		return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+	}
+
+	// Crear una variable para almacenar los datos leídos
+	var ebr structs.EBR
+
+	// Obtiene el primer ebr del archivo.
+	ebr.DeserializeEBR(discPath, int64(mbr.Mbr_partitions[ExtendidaIndex].Part_start))
+
+	validarEBR := false
+	lastnext := int32(-1)
+
+	// Escribir las particiones logicas en el archivo DOT
+	for ebr.Ebr_next != -1 {
+		validarEBR = true
+		// Calcular el Porcentaje en BYTES que ocupa la partición en el disco
+		logicaweight := float64(ebr.Ebr_size) * 100 / float64(mbr.Mbr_size)
+
+		// Escribir la partición en el archivo DOT
+		_, err := file.WriteString(`
+                <TD>EBR</TD>
+                <TD>Lógica<BR/><FONT POINT-SIZE="10" COLOR="red">` + fmt.Sprintf("%.2f", logicaweight) + `%` + ` del disco</FONT></TD>`)
+		if err != nil {
+			return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+		}
+
+		lastnext = ebr.Ebr_next
+
+		// Obtiene el proximo ebr del archivo
+		ebr.DeserializeEBR(discPath, int64(ebr.Ebr_next))
+	}
+
+	if !validarEBR {
+		// Escribir la partición en el archivo DOT
+		_, err := file.WriteString(`
+                <TD>EBR</TD>`)
+		if err != nil {
+			return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+		}
+	}
+
+	// Calcular el espacio libre en la ultima partición logica
+	ultimaParticionLogica := float64((mbr.Mbr_partitions[ExtendidaIndex].Part_size + mbr.Mbr_partitions[ExtendidaIndex].Part_start - lastnext)) * 100 / float64(mbr.Mbr_size)
+
+	_, err = file.WriteString(`
+		<TD>Libre<BR/><FONT POINT-SIZE="10" COLOR="red">` + fmt.Sprintf("%.2f", ultimaParticionLogica) + `%` + ` del disco</FONT></TD>`)
+	if err != nil {
+		return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+	}
+	// Cerrar la tabla
+	_, err = file.WriteString(`
+            </TR>
+        </TABLE>
+    >];
+}`)
+
+	if err != nil {
+		return fmt.Errorf("error al cerrar la tabla en el archivo DOT: %v", err)
+	}
+
+	return nil
+}
+
+// Función para generar el archivo DOT con la estructura del MBR
+func GenerateDotFileMBR(mbr structs.MBR, filePath string, discPath string) error {
 	// Crear el archivo .dot
 	file, err := os.Create(filePath)
 	if err != nil {

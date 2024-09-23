713b07908a5ce45f129d683afe62f8b78380f2b8
DominicRuano <dojuparupe1975@gmail.com>
2024-09-23 17:12:45 -0600

    Add block and inode bitmap reports, superblock report, and error handling improvements
    - Implementación del reporte de bitmap de bloques y bitmap de inodos.
    - Agregado el reporte del superbloque.
    - Nuevos casos añadidos al switch general de bifurcación para manejar los nuevos reportes.
    - Mejora en el manejo de errores en varias secciones del código.
    - Solución para permitir la generación correcta de imágenes en formato JPG.

--- backend/Commands/rep.go
+++ backend/Commands/rep.go
@@ -92,10 +92,364 @@ func Rep(tokens []string) string {
 		return MBRReporte(Cmd)
 	case "disk":
 		return DISKReporte(Cmd)
+	case "bm_inode":
+		return BMInodeReporte(Cmd)
+	case "bm_block":
+		return BMBloqueReporte(Cmd)
+	case "sb":
+		return sbReporte(Cmd)
 	default:
 		return fmt.Sprintf("Error: Nombre de reporte %s no reconocido.\n", Cmd.Rep_name)
 	}
 
+}
+
+func sbReporte(Cmd *Rep_st) string {
+	// Deserializa el mbr
+	mbr := &structs.MBR{}
+
+	// Lee el archivo binario
+	err := mbr.DeserializeMBR(Cmd.Rep_pathDisk)
+	if err != nil {
+		return fmt.Sprintf("Error: %s\n", err)
+	}
+
+	// Busca la particion
+	Particion := mbr.GetPartitionById(Cmd.Rep_id)
+	if Particion == nil {
+		return fmt.Sprintf("Error: Particion %s no encontrada.\n", Cmd.Rep_id)
+	}
+
+	// Obtiene el ofcet del superbloque
+	offsetSuperbloque := Particion.Part_start
+
+	// Crea una superblock para almacenar los datos leídos
+	superblock := &structs.SuperBloque{}
+
+	// Lee el superbloque
+	superblock.DeserializeSB(Cmd.Rep_pathDisk, int64(offsetSuperbloque))
+
+	// Obtener la extensión del archivo
+	ext := filepath.Ext(Cmd.Rep_pathDestino)
+
+	// Path final del archivo sin la extensión
+	finalPath := strings.TrimSuffix(Cmd.Rep_pathDestino, ext)
+
+	// Obtener el directorio del path (sin el archivo)
+	dir := filepath.Dir(Cmd.Rep_pathDestino)
+
+	// Crear todas las carpetas necesarias si no existen
+	err = os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return fmt.Sprintf("Error al crear directorios: %v\n", err)
+	}
+
+	// Generar el archivo DOT
+	err = GenerateDotFileSB(superblock, finalPath+".dot", Cmd.Rep_pathDisk)
+	if err != nil {
+		return fmt.Sprintln("Error:", err)
+	}
+
+	// Ejecutar el comando DOT
+	err = ExecuteDot(finalPath+".dot", Cmd.Rep_pathDestino)
+	if err != nil {
+		return fmt.Sprintln("Error:", err)
+	}
+
+	// Intentar eliminar el archivo
+	os.Remove(finalPath + ".dot")
+
+	// Mensaje de éxito
+	return fmt.Sprintf("REP: Reporte SB Para el Id %s generado con éxito.\n", Cmd.Rep_id)
+
+}
+
+// Función para generar el archivo DOT con la estructura del MBR
+func GenerateDotFileSB(sb *structs.SuperBloque, filePath string, discPath string) error {
+
+	// Crear el archivo .dot
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("error al crear el archivo DOT: %v", err)
+	}
+	defer file.Close()
+
+	disconame := filepath.Base(discPath)
+
+	// Escribir el contenido del archivo DOT
+	_, err = file.WriteString(`
+digraph G {
+	node [shape=plaintext];
+	A [label=< 
+		<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
+			<TR>
+				<TD COLSPAN="2" BGCOLOR="green"><B>Reporte de SUPERBLOQUE</B></TD>
+			</TR>
+			<TR>
+				<TD>Disco</TD>
+				<TD>` + disconame + `</TD>
+			</TR>
+			<TR>
+				<TD>s_filesystem_type</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_filesystem_type) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_inodes_count</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_inodes_count) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_blocks_count</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_blocks_count) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_free_blocks_count</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_free_blocks_count) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_free_inodes_count</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_free_inodes_count) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_mtime</TD>
+				<TD>` + time.Unix(int64(sb.SB_mtime), 0).Format("2006-01-02 15:04") + `</TD>
+			</TR>
+			<TR>
+				<TD>s_umtime</TD>
+				<TD>` + time.Unix(int64(sb.SB_umtime), 0).Format("2006-01-02 15:04") + `</TD>
+			</TR>
+			<TR>
+				<TD>s_mnt_count</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_mnt_count) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_magic</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_magic) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_inode_s</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_inode_size) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_block_s</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_block_size) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_first_ino</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_firs_ino) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_first_blo</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_first_blo) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_bm_inode_start</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_bm_inode_start) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_bm_block_start</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_bm_block_start) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_inode_start</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_inode_start) + `</TD>
+			</TR>
+			<TR>
+				<TD>s_block_start</TD>
+				<TD>` + fmt.Sprintf("%d", sb.SB_block_start) + `</TD>
+			</TR>
+			`)
+
+	if err != nil {
+		return fmt.Errorf("error al escribir en el archivo DOT: %v", err)
+	}
+
+	// Cerrar la tabla
+	_, err = file.WriteString(`
+        </TABLE>
+        >];
+}
+    `)
+	if err != nil {
+		return fmt.Errorf("error al cerrar la tabla en el archivo DOT: %v", err)
+	}
+
+	return nil
+}
+
+func BMBloqueReporte(Cmd *Rep_st) string {
+	// Deserializa el mbr
+	mbr := &structs.MBR{}
+
+	// Lee el archivo binario
+	err := mbr.DeserializeMBR(Cmd.Rep_pathDisk)
+	if err != nil {
+		return fmt.Sprintf("Error: %s\n", err)
+	}
+
+	// Busca la particion
+	Particion := mbr.GetPartitionById(Cmd.Rep_id)
+	if Particion == nil {
+		return fmt.Sprintf("Error: Particion %s no encontrada.\n", Cmd.Rep_id)
+	}
+
+	// Obtiene el ofcet del superbloque
+	offsetSuperbloque := Particion.Part_start
+
+	// Crea una superblock para almacenar los datos leídos
+	superblock := &structs.SuperBloque{}
+
+	// Lee el superbloque
+	superblock.DeserializeSB(Cmd.Rep_pathDisk, int64(offsetSuperbloque))
+
+	// Obtiene el ofcet del bitmap de inodos
+	offsetBitmapInodos := superblock.SB_bm_block_start
+
+	// Calcula el tamaño del bitmap
+	size := superblock.SB_free_blocks_count + superblock.SB_blocks_count
+
+	// Lee el bitmap de inodos
+	// Crea un bitmap para almacenar los datos leídos
+	bitmapInodos, err := utils.ReadBytes(Cmd.Rep_pathDisk, int64(offsetBitmapInodos), size)
+
+	// Verifica si hubo un error al leer el bitmap
+	if err != nil {
+		return fmt.Sprintf("Error: %s\n", err)
+	}
+
+	// Crea un archivo para almacenar el bitmap de inodos
+	file, err := os.Create(Cmd.Rep_pathDestino)
+	if err != nil {
+		return fmt.Sprintf("Error: %s\n", err)
+	}
+
+	counter := 0
+	for _, b := range bitmapInodos {
+		var charToWrite string
+
+		// Verificar si el byte es 0 o 1 y escribir el carácter correspondiente
+		if fmt.Sprintf("%d", b) == "88" {
+			charToWrite = "1"
+		} else if fmt.Sprintf("%d", b) == "79" {
+			charToWrite = "0"
+		} else {
+			// Manejar cualquier otro valor que no sea 0 o 1 si es necesario
+			charToWrite = "?"
+		}
+
+		// Escribir el carácter en el archivo
+		_, err = file.WriteString(charToWrite)
+		if err != nil {
+			return fmt.Sprintf("Error: %s\n", err)
+		}
+
+		counter++
+
+		// Cada 20 bytes, agregar un salto de línea
+		if counter%20 == 0 {
+			_, err = file.WriteString("\n")
+			if err != nil {
+				return fmt.Sprintf("Error: %s\n", err)
+			}
+		}
+	}
+
+	// Añadir un salto de línea al final, si es necesario (opcional)
+	if counter%20 != 0 {
+		_, err = file.WriteString("\n")
+		if err != nil {
+			return fmt.Sprintf("Error: %s\n", err)
+		}
+	}
+
+	return fmt.Sprintf("REP: Reporte BM Inode Para el Id %s generado con éxito.\n", Cmd.Rep_id)
+}
+
+func BMInodeReporte(Cmd *Rep_st) string {
+	// Deserializa el mbr
+	mbr := &structs.MBR{}
+
+	// Lee el archivo binario
+	err := mbr.DeserializeMBR(Cmd.Rep_pathDisk)
+	if err != nil {
+		return fmt.Sprintf("Error: %s\n", err)
+	}
+
+	// Busca la particion
+	Particion := mbr.GetPartitionById(Cmd.Rep_id)
+	if Particion == nil {
+		return fmt.Sprintf("Error: Particion %s no encontrada.\n", Cmd.Rep_id)
+	}
+
+	// Obtiene el ofcet del superbloque
+	offsetSuperbloque := Particion.Part_start
+
+	// Crea una superblock para almacenar los datos leídos
+	superblock := &structs.SuperBloque{}
+
+	// Lee el superbloque
+	superblock.DeserializeSB(Cmd.Rep_pathDisk, int64(offsetSuperbloque))
+
+	// Obtiene el ofcet del bitmap de inodos
+	offsetBitmapInodos := superblock.SB_bm_inode_start
+
+	// Calcula el tamaño del bitmap
+	size := superblock.SB_free_inodes_count + superblock.SB_inodes_count
+
+	// Lee el bitmap de inodos
+	// Crea un bitmap para almacenar los datos leídos
+	bitmapInodos, err := utils.ReadBytes(Cmd.Rep_pathDisk, int64(offsetBitmapInodos), size)
+
+	// Verifica si hubo un error al leer el bitmap
+	if err != nil {
+		return fmt.Sprintf("Error: %s\n", err)
+	}
+
+	// Crea un archivo para almacenar el bitmap de inodos
+	file, err := os.Create(Cmd.Rep_pathDestino)
+	if err != nil {
+		return fmt.Sprintf("Error: %s\n", err)
+	}
+
+	counter := 0
+	for _, b := range bitmapInodos {
+		var charToWrite string
+
+		// Verificar si el byte es 0 o 1 y escribir el carácter correspondiente
+		if fmt.Sprintf("%d", b) == "49" {
+			charToWrite = "1"
+		} else if fmt.Sprintf("%d", b) == "48" {
+			charToWrite = "0"
+		} else {
+			// Manejar cualquier otro valor que no sea 0 o 1 si es necesario
+			charToWrite = "?"
+		}
+
+		// Escribir el carácter en el archivo
+		_, err = file.WriteString(charToWrite)
+		if err != nil {
+			return fmt.Sprintf("Error: %s\n", err)
+		}
+
+		counter++
+
+		// Cada 20 bytes, agregar un salto de línea
+		if counter%20 == 0 {
+			_, err = file.WriteString("\n")
+			if err != nil {
+				return fmt.Sprintf("Error: %s\n", err)
+			}
+		}
+	}
+
+	// Añadir un salto de línea al final, si es necesario (opcional)
+	if counter%20 != 0 {
+		_, err = file.WriteString("\n")
+		if err != nil {
+			return fmt.Sprintf("Error: %s\n", err)
+		}
+	}
+
+	return fmt.Sprintf("REP: Reporte BM Inode Para el Id %s generado con éxito.\n", Cmd.Rep_id)
 }
 
 func DISKReporte(Cmd *Rep_st) string {
@@ -144,7 +498,10 @@ func MBRReporte(Cmd *Rep_st) string {
 	var mbr structs.MBR
 
 	// Obtiene el mbr del archivo
-	mbr.DeserializeMBR(Cmd.Rep_pathDisk)
+	err := mbr.DeserializeMBR(Cmd.Rep_pathDisk)
+	if err != nil {
+		return fmt.Sprintf("Error al obtener el MBR: %v\n", err)
+	}
 
 	// Obtener la extensión del archivo
 	ext := filepath.Ext(Cmd.Rep_pathDestino)
@@ -156,7 +513,7 @@ func MBRReporte(Cmd *Rep_st) string {
 	dir := filepath.Dir(Cmd.Rep_pathDestino)
 
 	// Crear todas las carpetas necesarias si no existen
-	err := os.MkdirAll(dir, os.ModePerm)
+	err = os.MkdirAll(dir, os.ModePerm)
 	if err != nil {
 		return fmt.Sprintf("Error al crear directorios: %v\n", err)
 	}
@@ -282,73 +639,87 @@ digraph G {
 
 	// Escribir en el archivo DOT
 	_, err = file.WriteString(`
-		    </TR>
-            <TR>`)
+		    </TR>`)
 	if err != nil {
 		return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
 	}
 
-	// Crear una variable para almacenar los datos leídos
-	var ebr structs.EBR
-
-	// Obtiene el primer ebr del archivo.
-	ebr.DeserializeEBR(discPath, int64(mbr.Mbr_partitions[ExtendidaIndex].Part_start))
-
-	validarEBR := false
-	lastnext := int32(-1)
-
-	// Escribir las particiones logicas en el archivo DOT
-	for ebr.Ebr_next != -1 {
-		validarEBR = true
-		// Calcular el Porcentaje en BYTES que ocupa la partición en el disco
-		logicaweight := float64(ebr.Ebr_size) * 100 / float64(mbr.Mbr_size)
-
-		// Calcular el Porcentaje en BYTES que ocupa el ebr en la partición extendida
-		ebrWeight := float64(unsafe.Sizeof(ebr)) * 100 / float64(mbr.Mbr_partitions[ExtendidaIndex].Part_size)
-
-		// Escribir la partición en el archivo DOT
-		_, err := file.WriteString(`
-                <TD>EBR<BR/><FONT POINT-SIZE="5" COLOR="blue">` + fmt.Sprintf("%.2f", ebrWeight) + `%` + ` del disco</FONT></TD>
-                <TD>Lógica<BR/><FONT POINT-SIZE="10" COLOR="red">` + fmt.Sprintf("%.2f", logicaweight) + `%` + ` del disco</FONT></TD>`)
-		if err != nil {
-			return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
-		}
-
-		lastnext = ebr.Ebr_next
-
-		// Obtiene el proximo ebr del archivo
-		ebr.DeserializeEBR(discPath, int64(ebr.Ebr_next))
-	}
-
-	if !validarEBR {
-		// Calcular el Porcentaje en BYTES que ocupa el ebr en la partición extendida
-		ebrWeight := float64(unsafe.Sizeof(ebr)) * 100 / float64(mbr.Mbr_partitions[ExtendidaIndex].Part_size)
-
-		// Escribir la partición en el archivo DOT
-		_, err := file.WriteString(`
-                <TD>EBR<BR/><FONT POINT-SIZE="5" COLOR="blue">` + fmt.Sprintf("%.2f", ebrWeight) + `%` + ` del disco</FONT></TD>`)
-		if err != nil {
-			return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
-		}
-	}
-
-	if lastnext == -1 {
-		lastnext = mbr.Mbr_partitions[ExtendidaIndex].Part_start
-	}
-
-	// Calcular el espacio libre en la ultima partición logica
-	ultimaParticionLogica := float64((mbr.Mbr_partitions[ExtendidaIndex].Part_size + mbr.Mbr_partitions[ExtendidaIndex].Part_start - lastnext)) * 100 / float64(mbr.Mbr_size)
-
-	if ultimaParticionLogica > 0.01 {
+	if ExtendidaIndex != -1 {
+		// Escribir en el archivo DOT
 		_, err = file.WriteString(`
-		<TD>Libre<BR/><FONT POINT-SIZE="10" COLOR="red">` + fmt.Sprintf("%.2f", ultimaParticionLogica) + `%` + ` del disco</FONT></TD>`)
+			<TR>`)
 		if err != nil {
 			return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
 		}
+		// Crear una variable para almacenar los datos leídos
+		var ebr structs.EBR
+
+		// Obtiene el primer ebr del archivo.
+		ebr.DeserializeEBR(discPath, int64(mbr.Mbr_partitions[ExtendidaIndex].Part_start))
+
+		validarEBR := false
+		lastnext := int32(-1)
+
+		// Escribir las particiones logicas en el archivo DOT
+		for ebr.Ebr_next != -1 {
+			validarEBR = true
+			// Calcular el Porcentaje en BYTES que ocupa la partición en el disco
+			logicaweight := float64(ebr.Ebr_size) * 100 / float64(mbr.Mbr_size)
+
+			// Calcular el Porcentaje en BYTES que ocupa el ebr en la partición extendida
+			ebrWeight := float64(unsafe.Sizeof(ebr)) * 100 / float64(mbr.Mbr_partitions[ExtendidaIndex].Part_size)
+
+			// Escribir la partición en el archivo DOT
+			_, err := file.WriteString(`
+					<TD>EBR<BR/><FONT POINT-SIZE="5" COLOR="blue">` + fmt.Sprintf("%.2f", ebrWeight) + `%` + ` del disco</FONT></TD>
+					<TD>Lógica<BR/><FONT POINT-SIZE="10" COLOR="red">` + fmt.Sprintf("%.2f", logicaweight) + `%` + ` del disco</FONT></TD>`)
+			if err != nil {
+				return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+			}
+
+			lastnext = ebr.Ebr_next
+
+			// Obtiene el proximo ebr del archivo
+			ebr.DeserializeEBR(discPath, int64(ebr.Ebr_next))
+		}
+
+		if !validarEBR {
+			// Calcular el Porcentaje en BYTES que ocupa el ebr en la partición extendida
+			ebrWeight := float64(unsafe.Sizeof(ebr)) * 100 / float64(mbr.Mbr_partitions[ExtendidaIndex].Part_size)
+
+			// Escribir la partición en el archivo DOT
+			_, err := file.WriteString(`
+					<TD>EBR<BR/><FONT POINT-SIZE="5" COLOR="blue">` + fmt.Sprintf("%.2f", ebrWeight) + `%` + ` del disco</FONT></TD>`)
+			if err != nil {
+				return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+			}
+		}
+
+		if lastnext == -1 {
+			lastnext = mbr.Mbr_partitions[ExtendidaIndex].Part_start
+		}
+
+		// Calcular el espacio libre en la ultima partición logica
+		ultimaParticionLogica := float64((mbr.Mbr_partitions[ExtendidaIndex].Part_size + mbr.Mbr_partitions[ExtendidaIndex].Part_start - lastnext)) * 100 / float64(mbr.Mbr_size)
+
+		if ultimaParticionLogica > 0.01 {
+			_, err = file.WriteString(`
+			<TD>Libre<BR/><FONT POINT-SIZE="10" COLOR="red">` + fmt.Sprintf("%.2f", ultimaParticionLogica) + `%` + ` del disco</FONT></TD>`)
+			if err != nil {
+				return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+			}
+		}
+
+		// Cerrar la tabla
+		_, err = file.WriteString(`
+	</TR>`)
+		if err != nil {
+			return fmt.Errorf("error al escribir el tamaño de la particion en el archivo DOT: %v", err)
+		}
+
 	}
 	// Cerrar la tabla
 	_, err = file.WriteString(`
-            </TR>
         </TABLE>
     >];
 }`)
@@ -391,6 +762,10 @@ digraph G {
 
 	// Escribir las particiones en el archivo DOT
 	for _, partition := range mbr.Mbr_partitions {
+		if partition.Part_start == -1 {
+			continue
+		}
+
 		if partition.Part_type[0] == 'E' {
 			// Convertir Part_name a string ignorando los caracteres nulos (0)
 			partName := ByteToString(partition.Part_name[:])
@@ -485,17 +860,38 @@ digraph G {
 	return nil
 }
 
-// Función para ejecutar el comando dot para generar una imagen en el formato especificado
 func ExecuteDot(dotFile, outputImage string) error {
 	// Obtener la extensión del archivo de salida (sin el punto inicial)
 	ext := filepath.Ext(outputImage)[1:]
 
-	// Ejecutar el comando `dot` con el formato dinámico (basado en la extensión del archivo de salida)
-	cmd := exec.Command("dot", "-T"+ext, dotFile, "-o", outputImage)
+	// Si el formato es jpg, primero generamos un archivo png temporal
+	tempImage := outputImage
+	if ext == "jpg" {
+		tempImage = outputImage[:len(outputImage)-len(ext)] + "png"
+	}
+
+	// Ejecutar el comando `dot` con el formato dinámico (basado en la extensión del archivo de salida o png si es jpg)
+	cmd := exec.Command("dot", "-T"+filepath.Ext(tempImage)[1:], dotFile, "-o", tempImage)
 	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("error al ejecutar el comando dot: %v", err)
 	}
+
+	// Si la extensión original es jpg, convertir el archivo png a jpg
+	if ext == "jpg" {
+		convertCmd := exec.Command("convert", tempImage, outputImage)
+		err = convertCmd.Run()
+		if err != nil {
+			return fmt.Errorf("error al convertir png a jpg: %v", err)
+		}
+
+		// Opcionalmente, eliminar el archivo png temporal
+		err = os.Remove(tempImage)
+		if err != nil {
+			return fmt.Errorf("error al eliminar el archivo temporal png: %v", err)
+		}
+	}
+
 	return nil
 }
 
